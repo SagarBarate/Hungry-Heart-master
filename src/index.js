c4fc1841b2405d256e533fc027186c6c5473d299
Sagar Barate <90105721+SagarBarate@users.noreply.github.com>
2023-07-20 23:58:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,37 +26,152 @@ const Header =() => {
 const styleCard ={
   backgroundColor :"#f0f0f0",
 }
-const RestaurantCard =() =>
+const RestaurantCard =(props) =>  
 {
+  const {resData} = props;
+
+  const {cloudinaryImageId,name,cuisines,avgRating} = resData?.data.info;
+
   return(
     <div className='res-card' style={styleCard}>
       <img 
       className="res-logo" 
       alt='res-logo'
-      src="https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani-750x750.jpg"/>
-
-      <h3>Meghna foods</h3>
-      <h4> Biryani, North Indian, Asian</h4>
-      <h4> 4.8 stars</h4>
+      src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+      <h3>{name}</h3>
+      <h4> {cuisines}</h4>
+      <h4> {avgRating}</h4>
       <h4>38 mintues</h4>
     </div>
   );
 }
 
-
+const resList =[
+  {
+  data :{
+    "info": {
+      "id": "23671",
+      "name": "McDonald's",
+      "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
+      "locality": "Bellasis Road",
+      "areaName": "Byculla",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Burgers",
+        "Beverages",
+        "Cafe",
+        "Desserts"
+      ],
+      "avgRating": 4.2,
+      "feeDetails": {
+        "restaurantId": "23671",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2000
+          },
+          {
+            "name": "BASE_TIME",
+            "fee": 1500
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 3500
+      },
+      "parentId": "630",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "10K+",
+      "sla": {
+        "deliveryTime": 20,
+        "lastMileTravel": 0.5,
+        "serviceability": "SERVICEABLE",
+        "slaString": "20 mins",
+        "lastMileTravelString": "0.5 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-07-21 00:45:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "10% OFF",
+        "subHeader": "ABOVE ₹999",
+        "discountTag": "FLAT DEAL"
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/restaurants/mcdonalds-bellasis-road-byculla-mumbai-23671",
+      "type": "WEBLINK"
+    }
+  }
+}
+];
 const Body =() =>
 {
   return(
     <div className='body'>
       <div className='search'>Search</div>
       <div className='res-container'>
-        <RestaurantCard />
-        <RestaurantCard />
-        
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.info.id} resData={restaurant}/>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 const AppLayout =()=>{
   return <div className='app'>
