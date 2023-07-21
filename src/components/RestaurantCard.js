12ad636c447432a7397b88da4d6629582f4b2316
Sagar Barate <sagarbarate2907@com.com>
2023-07-21 08:15:42 +0530

    first

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,25 @@
+import { CDN_URL } from "../utils/constant";
+
+const styleCard ={
+    backgroundColor :"#f0f0f0",
+  }
+  const RestaurantCard =(props) =>  
+  {
+    const {resData} = props;
+  
+    const {cloudinaryImageId,name,cuisines,avgRating} = resData?.data.info;
+  
+    return(
+      <div className='res-card' style={styleCard}>
+        <img 
+        className="res-logo" 
+        alt='res-logo'
+        src={CDN_URL+cloudinaryImageId}/>
+        <h3>{name}</h3>
+        <h4> {cuisines}</h4>
+        <h4> {avgRating}</h4>
+        <h4>38 mintues</h4>
+      </div>
+    );
+  }
+export default RestaurantCard;  
---
+++ src/utils/mockData.js
@@ -0,0 +1,116 @@
+
+const resList =[
+    {
+    data :{
+      "info": {
+        "id": "23671",
+        "name": "McDonald's",
+        "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
+        "locality": "Bellasis Road",
+        "areaName": "Byculla",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Burgers",
+          "Beverages",
+          "Cafe",
+          "Desserts"
+        ],
+        "avgRating": 4.2,
+        "feeDetails": {
+          "restaurantId": "23671",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 2000
+            },
+            {
+              "name": "BASE_TIME",
+              "fee": 1500
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3500
+        },
+        "parentId": "630",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 20,
+          "lastMileTravel": 0.5,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20 mins",
+          "lastMileTravelString": "0.5 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-07-21 00:45:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "10% OFF",
+          "subHeader": "ABOVE ₹999",
+          "discountTag": "FLAT DEAL"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/mcdonalds-bellasis-road-byculla-mumbai-23671",
+        "type": "WEBLINK"
+      }
+    }
+  }
+  ];
+
+export default resList;
