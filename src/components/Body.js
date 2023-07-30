2c2064a36b2c0db9eb6f9b7f933e10cb4e8f3eb4
Sagar Barate <90105721+SagarBarate@users.noreply.github.com>
2023-07-30 18:40:59 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,132 +3,59 @@ import resList from "../utils/mockData";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-const Body =() =>
-{
+const Body =() =>{
   const [listOfRestaurant, setListofRestaurant] = useState([]);
 
-//state variable - super powerful variable
-//     const [listOfRestaurant, setListofRestaurant] = useState([{data:{
-//       info:{
-//       "id": "23671",
-//         "name": "McDonald's",
-//         "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//         "locality": "Bellasis Road",
-//         "costForTwo": "₹400 for two",
-//         "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//         "avgRating": "4.2",
-//       }
-      
-//   }},{
-//     data:{
-//     info:{
-//       "id": "1234",
-//       "name": "KFC",
-//       "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//       "locality": "Bellasis Road",
-//       "costForTwo": "₹400 for two",
-//       "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//       "avgRating": "3.2",
-    
-//     },
-//    }
-//   },
-//   {
-//     data:{
-//     info:{
-//       "id": "1235",
-//       "name": "Macd",
-//       "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//       "locality": "Bellasis Road",
-//       "costForTwo": "₹400 for two",
-//       "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//       "avgRating": "4.1",
-//     },
-//    }
-//   }
-// ]);
+  const[filteredRestaurant, setfilteredRestaurant]=useState([])
 
-//normal javascript variable 
-// let listOfRestaurant = null;
+  const [searchText, setSearchText] = useState("");
 
-//   let listOfRestaurant =[
-//     {
-//     data:{
-//       info:{
-//       "id": "23671",
-//         "name": "McDonald's",
-//         "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//         "locality": "Bellasis Road",
-//         "costForTwo": "₹400 for two",
-//         "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//         "avgRating": "4.2",
-//       }
-      
-//   },
-// },
-//   {
-//     data:{
-//     info:{
-//       "id": "1234",
-//       "name": "KFC",
-//       "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//       "locality": "Bellasis Road",
-//       "costForTwo": "₹400 for two",
-//       "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//       "avgRating": "3.2",
-    
-//     },
-//    }
-//   },
-//   {
-//     data:{
-//     info:{
-//       "id": "1235",
-//       "name": "Macd",
-//       "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-//       "locality": "Bellasis Road",
-//       "costForTwo": "₹400 for two",
-//       "cuisines": ["Burgers","Beverages","Cafe","Desserts"],
-//       "avgRating": "4.1",
-//     },
-//    }
-//   }
-//   ];
-//
+  //whenever state variable update, react triggers a reconsalation cycle (re- renders the component)
 
-useEffect(()=>{
-  fetchData();
-  },[]);
+useEffect(()=>{fetchData();
+},[]);
 
 const fetchData = async() =>{
   const data = await fetch(
    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.969539&lng=72.819329&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
+);
 
     const json = await data.json();
     setListofRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-
-    
-    // setFilteredRestaurant(
-    //   json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    // );
-  
+    setfilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0].info.name);
 };
 
   return listOfRestaurant.length ===0 ? <Shimmer /> :(
     
     <div className='body'>
       <div className='filter'>
-        <button className="filter-btn" onClick={()=>{
+        <div className="search">
+          
+          <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+              setSearchText(e.target.value);
+          }}/>
 
-            //filter logic here 
+          <button onClick={()=>{
+            //Filter the restaurant cards and update the UI
+            //searchText
+            console.log(searchText);
+
+            const filteredRestaurant = listOfRestaurant.filter(
+              (res)=> { res.info.name.toLowerCase().includes(searchText.toLowerCase()) }
+                 
+              );
+              setfilteredRestaurant(filteredRestaurant);
+            }}>Search</button>
+
+        </div>
+
+        <button className="filter-btn" onClick={()=>{
             const filteredList = listOfRestaurant.filter(
+              (res) => res.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants.info.avgRatingString >4
+          );
             
-              (res) => res.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants.avgRatingString >4
-              
-            );
-            
-              setListofRestaurant(filteredList);
+           setListofRestaurant(filteredList);
         }}
         
         >
