6b7772ad54d0009ac556846ebdbae04d48c385d5
Sagar Barate <90105721+SagarBarate@users.noreply.github.com>
2023-08-03 23:17:34 +0530

    Create User.js

---
+++ src/components/User.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+
+const User = () => {
+
+useEffect (() =>{
+
+}, [])
+
+
+    return (
+        <div className="user-card">
+        <h2>Name: Sagar Barate</h2>
+        <h2>Location : Nav</h2>
+        <h2>Constact : 7045488409</h2>
+        </div>
+    )
+}
+
+export default User;
