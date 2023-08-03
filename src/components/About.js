1ee72fc2612295759ee4812f38617363ebbf17f9
Sagar Barate <90105721+SagarBarate@users.noreply.github.com>
2023-08-03 23:15:58 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return(
         <div>
-            <h1>About</h1>
+            <h1>About Class componets</h1>
+            <User name={"Sagar Barate(function)"}/>
+            <UserClass name={"Sagar Barte"} location={"Navi mumbai"} />
         </div>
     )
 }
+
+
 
 export default About;
