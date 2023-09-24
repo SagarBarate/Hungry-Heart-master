658a713b1ba5ac46e7c10c458d75cfdb7cd1b693
Sagar Barate <Sagarbarate2907@gmail.com>
2023-09-24 23:44:30 +0530

    Update RestaurantMenu.js

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,40 +2,35 @@ import React from 'react'
 import { useState ,useEffect } from 'react';
 import Shimmer from './Shimmer';
 import { useParams } from 'react-router-dom';
-import { MENU_API } from '../utils/constant';
+import useRestaurantMenu from '../utils/useRestaurantMenu';
+import RestaurantCategories from './RestaurantCategories';
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
 
   const { resId } = useParams();
 
-
-  useEffect( () => {
-    fetchMenu();
-}, []);
-const fetchMenu = async () =>
-{
-    const data = await fetch(MENU_API+resId);
-
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json.data);
-};
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) {
     return <Shimmer />; 
   }
 
-  const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
-  const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info || {};
+  const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card || {};
+  console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
-  console.log(itemCards);
+  const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
 
+  // console.log(categories);
   return (
-    <div className='menu'>
-      <h1>{name}</h1>
-      {cuisines && cuisines.length > 0 && <p>{cuisines.join(", ")} - {costForTwoMessage}</p>}
-      <ul>
+    <div className='text-center'>
+      <h1 className='font-bold my-10 text-2xl'>{name}</h1>
+      <p className='font-bold text-lg'>{cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      {/* Categories accoridian */}
+
+      {categories.map((category) => (<RestaurantCategories data={category?.card.card}/>))}
+      {/* <ul>
         {
         itemCards.map( item => (
         <li>
@@ -43,9 +38,7 @@ const fetchMenu = async () =>
         </li>
         ))
         }
-        
-        <li>Diet Coke</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
